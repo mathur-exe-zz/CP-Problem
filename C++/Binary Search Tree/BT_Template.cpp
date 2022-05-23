f5cdeaf9e574cc5240ffeb95d9eb79884cbc84f1
mathur-exe <gmathur1993@gmail.com>
2022-05-23 08:43:30 +0530

    Binary Tree Implementation in C++

---
+++ C++/Binary Search Tree/BT_Template.cpp
@@ -0,0 +1,35 @@
+#include "bit/stdc++.h"
+using namespace std;
+
+struct Node {
+	int data;
+	struct Node* left;
+	struct Node* right;
+
+	Node(int val) {
+		data = val;
+		left = NULL;
+		right = NULL 
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	struct Node* root = new Node(1);
+	root->left = new Node(2);
+	root->right = new Node(3);
+
+	root->left->left = new Node(4);
+	root->right->right = new Node(5);
+
+	/*
+			1
+		   / \
+		  2   3
+		 / \
+		4   5 
+	*/
+
+	return 0;
+}
